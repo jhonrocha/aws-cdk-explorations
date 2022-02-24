df574799eef60637daf6bf2ea0b24df06e1d2a56
Jhon Rocha <jhmrocha@gmail.com>
2022-02-24 15:30:12 -0300

    feat: CDK TS setup

---
+++ lib/aws-cdk-explorations-stack.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-useless-constructor */
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsCdkExplorationsStack extends Stack {
+  constructor (scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsCdkExplorationsQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
